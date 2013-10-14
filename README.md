6619517422136a873cf3154037fb42e9cf0803ac
Marc Sherry <msherry@gmail.com>
2013-10-13 17:23:50 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+payfac client
+=============
+
+Litle doesn't seem to provide a client for their Payfac API. This is an attempt to provide that.
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup
+setup(name='PayfacClient',
+      version='0.0.1',
+      description="Python client for Litle's Payfac API",
+      author='Marc Sherry',
+      author_email='marc@balancedpayments.com',
+      url='http://www.balancedpayments.com',
+      packages=['litleSdkPython'],
+      install_requires=[
+          'PyXB==1.2.3',
+      ],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Environment :: MacOS X'
+          'Environment :: Plugins'
+          'Environment :: Win32 (MS Windows)'
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'Intended Audience :: Information Technology',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent'
+          'Operating System :: MacOS',
+          'Operating System :: Microsoft',
+          'Operating System :: Unix',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Office/Business :: Financial',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      )
